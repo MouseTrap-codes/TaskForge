ef1df3dc1034c584e5da060bdb0722cc4eb212d1
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-07-16 22:07:57 -0500

    Implement logic for projects
    Each project takes in a name and adds todo items
    to an internal list. Each project's name can also
    be get and set, and the internal list can be
    exposed via a getter.

--- src/project.js
+++ src/project.js
@@ -1,3 +1,24 @@
-export function Project() {
-    
+import { todoItem } from "./todo-item.js";
+
+export function Project(projectName) {
+    let name = projectName;
+    let todoItemsList = [];
+
+    function addTodo(todoItem) {
+        if (todoItem.getProject() === name) {
+            todoItemsList.push(todoItem);
+        }
+    }
+
+    function getTodos() {
+        return todoItemsList;
+    }
+
+    const getName = () => name
+    const setName = (newName) => {name = newName};
+
+    return {
+        addTodo,
+        getTodos
+    }; 
 }
