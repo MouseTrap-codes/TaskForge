613dde878cf0670c9374e27e2e18ae5d28b95f77
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-07-18 11:11:42 -0500

    Implement orderBy name, priority, dueDate

--- src/order-by.js
+++ src/order-by.js
@@ -1,24 +1,33 @@
-import { PRIORITIES } from "./priorities";
+import { PRIORITIES_ORDER } from "./priorities";
+
 
 export function orderBy(items, fieldList) {
     if (fieldList.includes("dueDate") && field === "dueDate") {
         orderByDate(items);
     } else if (fieldList.includes("priority") && field === "priority") {
         orderByPriority(items);
+    } else if (field.List.includes("name") && field === "name") {
+        orderByName(items)
     }
 }
 
 // special logic for ordering by date
 function orderByDate(items) {
-    items.sort((a, b) => a.dueDate - b.dueDate);
+    items.sort((a, b) => b.getDueDate() - a.getDueDate()); // want descending order
     return items;
 }
 
 // todo: implement this
-function orderByPriority(items, field) {
+function orderByPriority(items) {
+    function compareFn(a, b) {
+        return PRIORITIES_ORDER(a.getPriority() - b.getPriority()); // want ascending order
+    }
+
+    items.sort(compareFn);
+
     return items;
 }
 
 function orderByName(items) {
-    items.sort((a, b) => a.name - b.name)
+    items.sort((a, b) => a.name - b.name) // just get lexicographic ordering
 }
--- src/priorities.js
+++ src/priorities.js
@@ -4,3 +4,10 @@ export const PRIORITIES = {
         MEDIUM: "medium",
         LOW: "low"
     }
+
+export const PRIORITIES_ORDER = {
+    [PRIORITIES.URGENT]: 1,
+    [PRIORITIES.HIGH]: 2,
+    [PRIORITIES.MEDIUM]: 3,
+    [PRIORITIES.LOW]: 4
+}
