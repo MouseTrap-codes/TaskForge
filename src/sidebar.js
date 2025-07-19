5a6e9eb66bc2fdf93bcf1eac215a8c8cf070b9f9
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-07-19 19:04:11 +0530

    Create file to implement searchbar logic

--- src/sidebar.js
+++ src/sidebar.js
@@ -5,7 +5,34 @@ export function sideBarDom(currentProject, projectList) {
     const sidebarDiv = document.createElement("div");
     sidebarDiv.classList.toggle("sidebar");
 
+    // logic for creating a new project
+
     // create searchbar
+    createSearchBar(content);
+}
+
+function createSearchBar(content) {
+    // create div for sidebar
+    const sidebarDiv = document.createElement("div");
+    sidebarDiv.classList.toggle("sidebar");
+
+    // logic for creating a new project
+
+    // create searchbar
+    const formDiv = document.createElement("div");
+    formDiv.classList.toggle("sidebar-searchbar");
     const form = document.createElement("form");
-    
+    form.action = "";
+    form.method = "get"
+    form.classList.toggle("searchbar");
+    const input = document.createElement("input");
+    input.type = "text";
+    input.name = "search";
+    input.id = "search";
+    input.textContent = "Search Projects";
+
+    form.appendChild(input);
+    formDiv.appendChild(form);
+    content.appendChild(formDiv);
+
 }
